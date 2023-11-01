ff333dc77fd9d2ac7d3c3bb7da5c4adc63dc77f6
Ali Soliman <alisolimanworks@gmail.com>
2023-11-01 10:49:38 +0200

    init Mui RTL

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import "./App.scss";
+import Typography from "@mui/material/Typography";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Typography variant="body1" color="initial">
+        يا مرحب بالشباب
+      </Typography>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,16 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
+
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
