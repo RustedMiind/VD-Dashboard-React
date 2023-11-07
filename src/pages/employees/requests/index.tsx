0b3089d663993bf2964dd895de7c484b4757bb73
Ali Soliman <alisolimanworks@gmail.com>
2023-11-07 16:47:48 +0200

    table added ro employees requests

--- src/pages/employees/requests/index.tsx
+++ src/pages/employees/requests/index.tsx
@@ -4,8 +4,23 @@ import {
   TextField,
   Button,
   IconButton,
+  Box,
+  Tabs,
+  Tab,
+  Paper,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableBody,
+  TableCell,
+  Chip,
 } from "@mui/material";
 import SearchBar from "./SearchBar";
+
+const temp = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
 
 function EmplyeesRequests() {
   return (
@@ -14,7 +29,45 @@ function EmplyeesRequests() {
         طلبات الموظفين
       </Typography>
       <SearchBar />
-      hello
+      <Box mt={2}>
+        <Tabs aria-label="basic tabs example">
+          <Tab label="الكل" />
+          <Tab label="الوارد" />
+          <Tab label="الصادر" />
+        </Tabs>
+      </Box>
+      <Paper variant="elevation" sx={{ bgcolor: "Background" }} elevation={4}>
+        <TableContainer sx={{ height: 500 }}>
+          <Table aria-label="simple table" stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell>رقم الطلب</TableCell>
+                <TableCell>اسم الموظف</TableCell>
+                <TableCell>تاريخ الورود</TableCell>
+                <TableCell>نوع الطلب</TableCell>
+                <TableCell>القسم</TableCell>
+                <TableCell>حالة الطلب</TableCell>
+                <TableCell>الملاحظات</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {temp.map((row, index) => (
+                <TableRow>
+                  <TableCell>{index}</TableCell>
+                  <TableCell>علي سليمان</TableCell>
+                  <TableCell>12/12/2012</TableCell>
+                  <TableCell>اداري</TableCell>
+                  <TableCell>في ميديا</TableCell>
+                  <TableCell>
+                    <Chip color="success" variant="outlined" label="نشط" />
+                  </TableCell>
+                  <TableCell>هذا النص هو مثال لنص يمكن..</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </Stack>
   );
 }
