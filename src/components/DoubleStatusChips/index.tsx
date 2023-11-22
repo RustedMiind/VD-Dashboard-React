9e7513c3f9cef3c1adf8aca6d66e15b951b730e1
Abdel-Rahman Fathy <105522928+Abdel-Rahman-Fathy@users.noreply.github.com>
2023-11-22 15:56:12 +0200

    Double Status Chips

---
+++ src/components/DoubleStatusChips/index.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "@mui/system";
+import StatusChip, { MuiMainColors } from "../StatusChip";
+
+function DoubleChips(props: PropsType) {
+  return (
+    <Stack
+      direction="row"
+      spacing={1}
+      alignItems="center"
+      mx={props.space ? 1 : undefined}
+    >
+      <StatusChip color={props.color} label={props.label} />
+      <StatusChip color={props.color} label={props.value} />
+    </Stack>
+  );
+}
+
+type PropsType = {
+  label: string;
+  value: string | number;
+  color: MuiMainColors;
+  space?: boolean;
+};
+
+export default DoubleChips;
