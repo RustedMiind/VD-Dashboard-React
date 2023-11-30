ad5c15fba24521f9595797812778717435f44770
Ali Soliman <alisolimanworks@gmail.com>
2023-11-30 15:32:26 +0200

    forcedownloadhandler function

---
+++ src/methods/forceDownloadHandler.tsx
@@ -0,0 +1,11 @@
+export function forceDownloadHandler(url: string) {
+  return () => {
+    const link = document.createElement("a");
+    link.href = url;
+    const fileName = url.substring(url.lastIndexOf("/") + 1);
+    link.setAttribute("download", fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+}
