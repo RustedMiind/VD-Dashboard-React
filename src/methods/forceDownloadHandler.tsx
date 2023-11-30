9c8b74ca7ff3107e91f7d1dcfc9361c57413bd05
Ali Soliman <alisolimanworks@gmail.com>
2023-11-30 15:36:34 +0200

    from function to class

--- src/methods/forceDownloadHandler.tsx
+++ src/methods/forceDownloadHandler.tsx
@@ -1,13 +1,12 @@
-export function forceDownloadHandler(url: string) {
-  return {
-    forceDownload: () => {
-      const link = document.createElement("a");
-      link.href = url;
-      const fileName = url.substring(url.lastIndexOf("/") + 1);
-      link.setAttribute("download", fileName);
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    },
-  };
+export class forceDownloadHandler {
+  constructor(private readonly url: string) {}
+  forceDownload() {
+    const link = document.createElement("a");
+    link.href = this.url;
+    const fileName = this.url.substring(this.url.lastIndexOf("/") + 1);
+    link.setAttribute("download", fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
