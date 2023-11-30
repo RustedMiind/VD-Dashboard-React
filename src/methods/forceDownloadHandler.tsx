7dcd5ff37941508a683be7d39912410f97651336
Ali Soliman <alisolimanworks@gmail.com>
2023-11-30 15:33:35 +0200

    force download returns an object

--- src/methods/forceDownloadHandler.tsx
+++ src/methods/forceDownloadHandler.tsx
@@ -1,11 +1,13 @@
 export function forceDownloadHandler(url: string) {
-  return () => {
-    const link = document.createElement("a");
-    link.href = url;
-    const fileName = url.substring(url.lastIndexOf("/") + 1);
-    link.setAttribute("download", fileName);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  return {
+    forceDownload: () => {
+      const link = document.createElement("a");
+      link.href = url;
+      const fileName = url.substring(url.lastIndexOf("/") + 1);
+      link.setAttribute("download", fileName);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    },
   };
 }
