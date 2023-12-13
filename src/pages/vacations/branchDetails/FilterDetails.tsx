1ba94879298236c6d9d6a1ca2f8d8a6e755cf409
Abdel-Rahman Fathy <105522928+Abdel-Rahman-Fathy@users.noreply.github.com>
2023-12-13 13:20:49 +0200

    Fixing merge conflicts

--- src/pages/vacations/branchDetails/FilterDetails.tsx
+++ src/pages/vacations/branchDetails/FilterDetails.tsx
@@ -1,14 +1,22 @@
 import { Button, Grid, MenuItem, TextField } from "@mui/material";
 import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
 import { DatePicker } from "@mui/x-date-pickers";
-import AddDialog from "../AddDialog";
 import { useState } from "react";
-import { useParams } from "react-router";
+import AddDialog from "../AddDialog";
 import ErrorDialog from "../ErrorDialog";
+import { useParams } from "react-router-dom";
 
 function FilterDetails() {
-  const { branchId } = useParams();
+  const [yearFilter, setYearFilter] = useState();
+  const [statusFilter, setStatusFilter] = useState<{
+    name: string;
+    value: Number;
+  }>({
+    name: "string",
+    value: 0,
+  });
 
+  const { branchId } = useParams();
   const [dialogState, setDialogState] = useState<DialogState>("none");
   const closeDialog = () => {
     setDialogState("none");
@@ -23,85 +31,75 @@ function FilterDetails() {
     setDialogState("details");
   };
 
-  function FilterDetails() {
-    const [yearFilter, setYearFilter] = useState();
-    const [statusFilter, setStatusFilter] = useState<{
-      name: string;
-      value: Number;
-    }>({
-      name: "string",
-      value: 0,
-    });
-
-    return (
-      <Grid container width={1} py={2}>
-        <Grid container spacing={2} md={10}>
-          <Grid item xs={2}>
-            <DatePicker
-              slotProps={{ textField: { fullWidth: true, size: "small" } }}
-              label={'"العام"'}
-              openTo="year"
-              disableFuture
-              views={["year"]}
-              value={yearFilter}
-              // onChange={(e) => {
-              //   setYearFilter(e.target.value);
-              // }}
-            />
-          </Grid>
-          <Grid item xs={2}>
-            <TextField
-              select
-              fullWidth
-              label="الحالة"
-              size="small"
-              sx={{ color: "primary" }}
-              value={statusFilter.value}
-              onChange={(e) => {
-                setStatusFilter({
-                  ...statusFilter,
-                  value: parseInt(e.target.value),
-                });
-              }}
-            >
-              <MenuItem value={10}>معتمد</MenuItem>
-              <MenuItem value={20}>مسودة</MenuItem>
-            </TextField>
-          </Grid>
-        </Grid>
-
-        <Grid item md={2}>
-          <Button
-            variant="contained"
-            size="large"
-            color="primary"
-            fullWidth
-            onClick={() => {
-              openAddDialog();
-            }}
-          >
-            <AddCircleOutlineRoundedIcon sx={{ mr: 1 }} />
-            إضافة محدد
-          </Button>
-          {branchId && (
-            <AddDialog
-              open={dialogState === "add"}
-              branch_id={branchId}
-              onClose={closeDialog}
-              openErrorDialog={openErrorDialog}
-              openAddDialog={openAddDialog}
-            />
-          )}
-          <ErrorDialog
-            open={dialogState === "error"}
-            onClose={openAddDialog}
-            massage={"error message"}
+  return (
+    <Grid container width={1} py={2}>
+      <Grid container spacing={2} md={10}>
+        <Grid item xs={2}>
+          <DatePicker
+            slotProps={{ textField: { fullWidth: true, size: "small" } }}
+            label={'"العام"'}
+            openTo="year"
+            disableFuture
+            views={["year"]}
+            value={yearFilter}
+            // onChange={(e) => {
+            //   setYearFilter(e.target.value);
+            // }}
           />
         </Grid>
+        <Grid item xs={2}>
+          <TextField
+            select
+            fullWidth
+            label="الحالة"
+            size="small"
+            sx={{ color: "primary" }}
+            value={statusFilter.value}
+            onChange={(e) => {
+              setStatusFilter({
+                ...statusFilter,
+                value: parseInt(e.target.value),
+              });
+            }}
+          >
+            <MenuItem value={10}>معتمد</MenuItem>
+            <MenuItem value={20}>مسودة</MenuItem>
+          </TextField>
+        </Grid>
       </Grid>
-    );
-  }
+
+      <Grid item md={2}>
+        <Button
+          variant="contained"
+          size="large"
+          color="primary"
+          fullWidth
+          onClick={() => {
+            openAddDialog();
+          }}
+        >
+          <AddCircleOutlineRoundedIcon sx={{ mr: 1 }} />
+          إضافة محدد
+        </Button>
+      </Grid>
+      {branchId && (
+        <AddDialog
+          open={dialogState === "add"}
+          branch_id={branchId}
+          onClose={closeDialog}
+          openErrorDialog={openErrorDialog}
+          openAddDialog={openAddDialog}
+        />
+      )}
+      <ErrorDialog
+        open={dialogState === "error"}
+        onClose={openAddDialog}
+        massage={"error message"}
+      />
+    </Grid>
+  );
 }
-export default FilterDetails;
 
 export type DialogState = "none" | "error" | "add" | "details";
+
+export default FilterDetails;
