9f605eac867763d3642ced3c950d3cbe1e88965b
Abdel-Rahman Fathy <105522928+Abdel-Rahman-Fathy@users.noreply.github.com>
2023-12-18 15:11:54 +0200

    add post id and toaster and print

--- src/pages/vacations/VacationDetails/components/Table.tsx
+++ src/pages/vacations/VacationDetails/components/Table.tsx
@@ -11,13 +11,17 @@ import {
 } from "@mui/material";
 import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
 import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
-import { useState } from "react";
-import SetDialog from "../SetDialog";
-import { EmployeeType, Vacation } from "../../../../types";
+
+import { Vacation } from "../../../../types";
 import { formatDate, getDateDiff } from "../../../../methods";
 import { convertMsToDays } from "../../../../methods/conversions/msToDays";
+import { NotPrintableTableCell } from "../../../clients/data/Table";
+import { useEffect, useRef } from "react";
 
 const TableData = (props: PropsType) => {
+  const tableRef: React.RefObject<HTMLTableElement> =
+    useRef<HTMLTableElement>(null);
+
   return (
     <>
       <Paper>
@@ -26,14 +30,16 @@ const TableData = (props: PropsType) => {
             minHeight: 500,
           }}
         >
-          <Table aria-label="simple table">
+          <Table aria-label="simple table" ref={tableRef}>
             <TableHead>
               <TableRow>
                 <TableCell>اسم الاجازة</TableCell>
                 <TableCell>عدد الايام </TableCell>
                 <TableCell>تاريخ البداية</TableCell>
                 <TableCell>تاريخ النهاية</TableCell>
-                <TableCell>الاعدادات</TableCell>
+                <NotPrintableTableCell>
+                  <TableCell>الاعدادات</TableCell>
+                </NotPrintableTableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -50,16 +56,18 @@ const TableData = (props: PropsType) => {
                   </TableCell>
                   <TableCell>{formatDate(vacation.date_from)}</TableCell>
                   <TableCell>{formatDate(vacation.date_to)}</TableCell>
-                  <TableCell>
-                    <Stack direction={"row"} gap={1}>
-                      <IconButton onClick={props.openUpdateDialog(vacation)}>
-                        <BorderColorOutlinedIcon />
-                      </IconButton>
-                      <IconButton color="primary">
-                        <RemoveRedEyeOutlinedIcon />
-                      </IconButton>
-                    </Stack>
-                  </TableCell>
+                  <NotPrintableTableCell>
+                    <TableCell>
+                      <Stack direction={"row"} gap={1}>
+                        <IconButton onClick={props.openUpdateDialog(vacation)}>
+                          <BorderColorOutlinedIcon />
+                        </IconButton>
+                        <IconButton color="primary">
+                          <RemoveRedEyeOutlinedIcon />
+                        </IconButton>
+                      </Stack>
+                    </TableCell>
+                  </NotPrintableTableCell>
                 </TableRow>
               ))}
             </TableBody>
--- src/pages/vacations/VacationDetails/index.tsx
+++ src/pages/vacations/VacationDetails/index.tsx
@@ -1,8 +1,15 @@
-import { Button, Paper, Stack, Typography } from "@mui/material";
+import {
+  Alert,
+  Button,
+  Paper,
+  Snackbar,
+  Stack,
+  Typography,
+} from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import TableData from "./components/Table";
 import PrintIcon from "@mui/icons-material/Print";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import SetDialog from "./SetDialog";
 import axios from "axios";
 import { Api } from "../../../constants";
@@ -10,6 +17,8 @@ import { useParams } from "react-router-dom";
 import { EmployeeType, Vacation } from "../../../types";
 import LoadingTable from "../../../components/LoadingTable";
 import { BreadCrumbContext } from "../../../layout/main-layout/BreadCrumbContext";
+import { LoadingButton } from "@mui/lab";
+import { ToasterType } from "../../../types/other/ToasterStateType";
 
 const VacationsTable = () => {
   const { yearId, branchId } = useParams();
@@ -23,11 +32,53 @@ const VacationsTable = () => {
   const [employeesInBranch, setemployeesInBranch] = useState<
     EmployeeType[] | undefined
   >();
+  const tableRef: React.RefObject<HTMLTableElement> =
+    useRef<HTMLTableElement>(null);
+  const handlePrint = () => {
+    if (tableRef?.current) {
+      window.print();
+    }
+  };
   const [vacations, setVacations] = useState<"loading" | "error" | Vacation[]>(
     "loading"
   );
-
-  console.log(employeesInBranch);
+  const [sendState, setSendState] = useState<
+    "loading" | "error" | "success" | "none"
+  >("none");
+  const [toaster, setToaster] = useState<ToasterType>({
+    open: false,
+    message: "",
+    severity: "success",
+  });
+  function updateToaster(partial: Partial<ToasterType>) {
+    setToaster({ ...toaster, ...partial });
+  }
+  function updateAndOpenToaster(partial: Partial<ToasterType>) {
+    updateToaster({ ...partial, open: true });
+  }
+  function handleCloseToaster() {
+    updateToaster({ open: false });
+  }
+  function Delete() {
+    setSendState("loading");
+    axios
+      .post(Api(`employee/vacation-day/${yearId}`), {})
+      .then((res) => {
+        console.log(res);
+        setSendState("success");
+        updateAndOpenToaster({
+          severity: "success",
+          message: "تم اعتماد محدد اجازات السنويه بنجاح",
+        });
+      })
+      .catch((err) => {
+        setSendState("error");
+        updateAndOpenToaster({
+          severity: "error",
+          message: "تعذر في اعتماد محدد اجازات السنويه",
+        });
+      });
+  }
 
   useEffect(() => {
     getEmployeeRequest();
@@ -78,6 +129,7 @@ const VacationsTable = () => {
           startIcon={<PrintIcon />}
           color="primary"
           variant="outlined"
+          onClick={handlePrint}
           sx={{
             mt: 1,
             width: "fit-content",
@@ -88,7 +140,8 @@ const VacationsTable = () => {
         >
           طباعة
         </Button>
-        <Button
+        <LoadingButton
+          loading={sendState === "loading"}
           variant="contained"
           sx={{
             mt: 1,
@@ -97,9 +150,10 @@ const VacationsTable = () => {
             px: 5,
             py: 1,
           }}
+          onClick={Delete}
         >
           حفظ
-        </Button>
+        </LoadingButton>
       </Stack>
       <SetDialog
         setTableDate={setTableData}
@@ -109,6 +163,20 @@ const VacationsTable = () => {
         title="اضافة اجازة"
         employeesInBranch={employeesInBranch}
       />
+      <Snackbar
+        open={toaster.open}
+        autoHideDuration={6000}
+        onClose={handleCloseToaster}
+        // action={action}
+      >
+        <Alert
+          onClose={handleCloseToaster}
+          severity={toaster.severity}
+          sx={{ width: "100%" }}
+        >
+          {toaster.message}
+        </Alert>
+      </Snackbar>
     </Stack>
   );
 
