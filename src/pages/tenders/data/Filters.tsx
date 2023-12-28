41ded99cd0a8e0653616a929897aebf5f2bddbce
Ali Soliman <alisolimanworks@gmail.com>
2023-12-29 00:10:42 +0200

    Tender Filters component

---
+++ src/pages/tenders/data/Filters.tsx
@@ -0,0 +1,52 @@
+import { Box, Button, Grid, TextField } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
+
+function GridItem({ children }: { children: React.ReactNode }) {
+  return (
+    <Grid item xs={6} lg={2} md={4}>
+      {children}
+    </Grid>
+  );
+}
+
+function TendersFilters() {
+  return (
+    <Box component="form" pb={7}>
+      <Grid container spacing={2}>
+        <GridItem>
+          <TextField
+            fullWidth
+            label="اسم جهة المنافسة"
+            type="text"
+            size="small"
+          />
+        </GridItem>
+        <GridItem>
+          <TextField fullWidth label="رقم الجهة" type="number" size="small" />
+        </GridItem>
+        <GridItem>
+          <TextField fullWidth label="اسم المنافسة" type="text" size="small" />
+        </GridItem>
+        <GridItem>
+          <DatePicker
+            slotProps={{ textField: { fullWidth: true, size: "small" } }}
+            label={"تاريخ التقديم"}
+          />
+        </GridItem>
+        <GridItem>
+          <DatePicker
+            slotProps={{ textField: { fullWidth: true, size: "small" } }}
+            label={"تاريخ الانتهاء"}
+          />
+        </GridItem>
+        <GridItem>
+          <Button variant="contained" fullWidth>
+            بحث
+          </Button>
+        </GridItem>
+      </Grid>
+    </Box>
+  );
+}
+
+export default TendersFilters;
--- src/pages/tenders/data/index.tsx
+++ src/pages/tenders/data/index.tsx
@@ -1,10 +1,12 @@
 import { Paper, Stack } from "@mui/material";
 import ControlSection from "./ControlSection";
 import TendersTable from "./Table";
+import TendersFilters from "./Filters";
 
 function TendersData() {
   return (
     <Stack>
+      <TendersFilters />
       <Paper>
         <ControlSection />
         {/*   - tender statuses   */}
