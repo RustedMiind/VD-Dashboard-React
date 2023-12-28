61c7bf6dd4827f4d5b2b34baaf6823bf87b3df7c
Ali Soliman <alisolimanworks@gmail.com>
2023-12-28 14:05:51 +0200

    update in icons in control section

--- src/pages/tenders/data/ControlSection.tsx
+++ src/pages/tenders/data/ControlSection.tsx
@@ -1,13 +1,16 @@
 import { Box, Button, Stack } from "@mui/material";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import DeleteIcon from "@mui/icons-material/Delete";
-import CreditScoreIcon from "@mui/icons-material/CreditScore";
+import EditIcon from "@mui/icons-material/Edit";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 function ControlSection() {
   return (
     <Stack direction={"row"} p={2} justifyContent={"space-between"}>
       <Box>
-        <Button variant="contained">إضافة منافسة</Button>
+        <Button startIcon={<AddCircleOutlineIcon />} variant="contained">
+          إضافة منافسة
+        </Button>
       </Box>
       <Stack direction={"row"} spacing={2}>
         <Button variant="outlined" startIcon={<FilterAltIcon />}>
@@ -21,7 +24,7 @@ function ControlSection() {
         >
           حذف
         </Button>
-        <Button variant="outlined" startIcon={<CreditScoreIcon />}>
+        <Button variant="outlined" startIcon={<EditIcon />}>
           تعديل
         </Button>
       </Stack>
