3caf43503a9593cfbdbbfec261c6a35d4b5e40b0
Ali Soliman <alisolimanworks@gmail.com>
2023-12-28 15:12:26 +0200

    Tenders Data Table

--- src/pages/tenders/data/index.tsx
+++ src/pages/tenders/data/index.tsx
@@ -1,11 +1,13 @@
 import { Paper, Stack } from "@mui/material";
 import ControlSection from "./ControlSection";
+import TendersTable from "./Table";
 
 function TendersData() {
   return (
     <Stack>
       <Paper>
         <ControlSection />
+        <TendersTable />
         {/*   - tender statuses   */}
         {/*   - tenders table   */}
         {/* end paper */}
