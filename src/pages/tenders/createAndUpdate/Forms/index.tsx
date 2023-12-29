c3f3fb6251c01e61b6bdda73b2c85b2465e74770
Ali Soliman <alisolimanworks@gmail.com>
2023-12-29 17:43:47 +0200

    Tender MainDataForm

--- src/pages/tenders/createAndUpdate/Forms/index.tsx
+++ src/pages/tenders/createAndUpdate/Forms/index.tsx
@@ -1,10 +1,12 @@
 import Typography from "@mui/material/Typography";
+import MainDataForm from "./MainDataForm";
+import { SeparatedAccordion } from "../../../../components/SeparatedAccordion";
 function FormsSection() {
   return (
     <>
-      <Typography variant="body1" color="initial">
-        Form 1
-      </Typography>
+      <SeparatedAccordion title="بيانات المنافسة">
+        <MainDataForm />
+      </SeparatedAccordion>
       <Typography variant="body1" color="initial">
         Form 2
       </Typography>
