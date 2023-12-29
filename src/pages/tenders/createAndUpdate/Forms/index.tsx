7455decc91ca953cceeea538c2d352fca7e9c992
Ali Soliman <alisolimanworks@gmail.com>
2023-12-29 22:49:45 +0200

    Managers Form Section

--- src/pages/tenders/createAndUpdate/Forms/index.tsx
+++ src/pages/tenders/createAndUpdate/Forms/index.tsx
@@ -1,22 +1,25 @@
 import Typography from "@mui/material/Typography";
 import MainDataForm from "./MainDataForm";
 import { SeparatedAccordion } from "../../../../components/SeparatedAccordion";
+import ManagersForm from "./ManagersForm";
+import { Stack } from "@mui/material";
 function FormsSection() {
   return (
-    <>
+    <Stack spacing={2}>
       <SeparatedAccordion title="بيانات المنافسة">
         <MainDataForm />
       </SeparatedAccordion>
-      <Typography variant="body1" color="initial">
-        Form 2
-      </Typography>
+
+      <SeparatedAccordion title="مهام المنافسة (المسؤولين عن المنافسة)">
+        <ManagersForm />
+      </SeparatedAccordion>
       <Typography variant="body1" color="initial">
         Form 3
       </Typography>
       <Typography variant="body1" color="initial">
         Form 4
       </Typography>
-    </>
+    </Stack>
   );
 }
 
