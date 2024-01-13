7907ec9fac8ebba7d2b81b92719028335dae7719
Abdel-Rahman Fathy <105522928+Abdel-Rahman-Fathy@users.noreply.github.com>
2024-01-13 03:26:00 +0200

    add gradient

--- src/pages/tenders/controlPanel/index.tsx
+++ src/pages/tenders/controlPanel/index.tsx
@@ -1,10 +1,14 @@
 import ControlPanelContextProvider from "./controlPanelContext";
 import ContentPanal from "./ContentPanal";
+import GradientBg from "../../../components/GradientBg";
+import { Typography } from "@mui/material";
 
 export default function ControlPanal() {
   return (
     <ControlPanelContextProvider>
-      <ContentPanal />
+      <GradientBg>
+        <Typography>abdo</Typography>
+      </GradientBg>
     </ControlPanelContextProvider>
   );
 }
