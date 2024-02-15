2434a71affc13c9369350ead858773e0c8aab55f
Abdel-Rahman Fathy <105522928+Abdel-Rahman-Fathy@users.noreply.github.com>
2024-02-15 12:56:41 +0200

    add to update in items soil

--- src/pages/soilSection/details/Tabs/Views/ItemsView/Dialogs/reportDialog/index.tsx
+++ src/pages/soilSection/details/Tabs/Views/ItemsView/Dialogs/reportDialog/index.tsx
@@ -115,7 +115,6 @@ export default function ReportDialog({ open, onClose, id }: PropsType) {
               >
                 <MenuItem value={18}>مقبول</MenuItem>
                 <MenuItem value={19}>مرفوض</MenuItem>
-                <MenuItem value={33}>معتمد</MenuItem>
               </Select>
             </FormControl>
           </GridItem>
