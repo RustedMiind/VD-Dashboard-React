69e1ce6e7e52fec8bceb5c3783ab9fc6d90f6bc5
rustedmiind <alisolimanworks@gmail.com>
2024-02-24 11:49:14 +0200

    Activate search bar in design requests page

--- src/pages/designs/DesignsData/Views/index.tsx
+++ src/pages/designs/DesignsData/Views/index.tsx
@@ -1,26 +1,29 @@
 import { Stack } from "@mui/material";
 import SearchBar from "../SearchBar";
 import CenteredPagination from "../../../../components/CenteredPagination";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import DesignProjectsView from "./DesignProjectsView";
+import BuyRequestsView from "./BuyRequestsView";
 
-function Views() {
-  const [search, setSearch] = useState("");
-  function handleSearch() {
-    console.log(search);
-  }
-  return (
-    <Stack spacing={4}>
-      <SearchBar
-        search={search}
-        setSearch={setSearch}
-        handleSearch={handleSearch}
-      />
-      {/* Show Data in Table  */}
-      <DesignProjectsView />
-      {/* pagination */}
-    </Stack>
-  );
+function Views({ currentPage }: PropsType) {
+  let toShow: JSX.Element = useMemo(() => {
+    switch (currentPage) {
+      case 1:
+        return <DesignProjectsView />;
+
+      case 2:
+        return <BuyRequestsView />;
+
+      default:
+        return <DesignProjectsView />;
+    }
+  }, [currentPage]);
+
+  return <Stack spacing={4}>{toShow}</Stack>;
 }
+
+type PropsType = {
+  currentPage: number;
+};
 
 export default Views;
