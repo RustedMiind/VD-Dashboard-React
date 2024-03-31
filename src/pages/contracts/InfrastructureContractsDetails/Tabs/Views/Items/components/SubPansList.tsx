2f29229796fa7195a2bb90cfa7caffa387ecfdd9
Sallam Rady <sallamrady@gmail.com>
2024-03-31 21:20:18 +0200

    decrease bos shadow of subPand Btn

--- src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/Items/components/SubPansList.tsx
+++ src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/Items/components/SubPansList.tsx
@@ -2,6 +2,33 @@ import { Box, Grid, Typography } from "@mui/material";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 export default function SubPandsList() {
+  let SingleBtn = () => (
+    <Box
+      display={"flex"}
+      alignItems="center"
+      width={"90%"}
+      minHeight={"62px"}
+      borderRadius={"12px"}
+      boxShadow={"0.5px 0.5px 1px 1px lightgray"}
+      marginY={"10px"}
+      sx={{
+        cursor: "pointer",
+      }}
+    >
+      <KeyboardBackspaceIcon
+        sx={{
+          bgcolor: `primary.main`,
+          color: `#fff`,
+          borderRadius: "50%",
+          fontSize: "1rem",
+          marginX: "12px",
+        }}
+      />
+      <Typography variant="h6" fontWeight={700}>
+        بند فرعي مثال 2
+      </Typography>
+    </Box>
+  );
   return (
     <Grid
       item
@@ -15,162 +42,13 @@ export default function SubPandsList() {
         padding: " 1rem 10px",
         display: "flex",
         alignItems: "start",
-        justifyContent: "center",
+        justifyContent: "start",
         flexDirection: "column",
       }}
     >
-      {/* original Btn */}
-      <Box
-        display={"flex"}
-        alignItems="center"
-        width={"90%"}
-        minHeight={"62px"}
-        borderRadius={"12px"}
-        boxShadow={"1px 1px 2px 2px lightgray"}
-        marginY={"10px"}
-        sx={{
-          cursor: "pointer",
-        }}
-      >
-        <KeyboardBackspaceIcon
-          sx={{
-            bgcolor: `primary.main`,
-            color: `#fff`,
-            borderRadius: "50%",
-            fontSize: "1rem",
-            marginX: "12px",
-          }}
-        />
-        <Typography variant="h6" fontWeight={700}>
-          بند فرعي مثال 2
-        </Typography>
-      </Box>
-      {/* copies */}
-      <Box
-        display={"flex"}
-        alignItems="center"
-        width={"90%"}
-        minHeight={"62px"}
-        borderRadius={"12px"}
-        boxShadow={"1px 1px 2px 2px lightgray"}
-        marginY={"10px"}
-        sx={{
-          cursor: "pointer",
-        }}
-      >
-        <KeyboardBackspaceIcon
-          sx={{
-            bgcolor: `primary.main`,
-            color: `#fff`,
-            borderRadius: "50%",
-            fontSize: "1rem",
-            marginX: "12px",
-          }}
-        />
-        <Typography variant="h6" fontWeight={700}>
-          بند فرعي مثال 2
-        </Typography>
-      </Box>
-      <Box
-        display={"flex"}
-        alignItems="center"
-        width={"90%"}
-        minHeight={"62px"}
-        borderRadius={"12px"}
-        boxShadow={"1px 1px 2px 2px lightgray"}
-        marginY={"10px"}
-        sx={{
-          cursor: "pointer",
-        }}
-      >
-        <KeyboardBackspaceIcon
-          sx={{
-            bgcolor: `primary.main`,
-            color: `#fff`,
-            borderRadius: "50%",
-            fontSize: "1rem",
-            marginX: "12px",
-          }}
-        />
-        <Typography variant="h6" fontWeight={700}>
-          بند فرعي مثال 2
-        </Typography>
-      </Box>
-      <Box
-        display={"flex"}
-        alignItems="center"
-        width={"90%"}
-        minHeight={"62px"}
-        borderRadius={"12px"}
-        boxShadow={"1px 1px 2px 2px lightgray"}
-        marginY={"10px"}
-        sx={{
-          cursor: "pointer",
-        }}
-      >
-        <KeyboardBackspaceIcon
-          sx={{
-            bgcolor: `primary.main`,
-            color: `#fff`,
-            borderRadius: "50%",
-            fontSize: "1rem",
-            marginX: "12px",
-          }}
-        />
-        <Typography variant="h6" fontWeight={700}>
-          بند فرعي مثال 2
-        </Typography>
-      </Box>
-      <Box
-        display={"flex"}
-        alignItems="center"
-        width={"90%"}
-        minHeight={"62px"}
-        borderRadius={"12px"}
-        boxShadow={"1px 1px 2px 2px lightgray"}
-        marginY={"10px"}
-        sx={{
-          cursor: "pointer",
-        }}
-      >
-        <KeyboardBackspaceIcon
-          sx={{
-            bgcolor: `primary.main`,
-            color: `#fff`,
-            borderRadius: "50%",
-            fontSize: "1rem",
-            marginX: "12px",
-          }}
-        />
-        <Typography variant="h6" fontWeight={700}>
-          بند فرعي مثال 2
-        </Typography>
-      </Box>
-      <Box
-        display={"flex"}
-        alignItems="center"
-        width={"90%"}
-        minHeight={"62px"}
-        borderRadius={"12px"}
-        boxShadow={"1px 1px 2px 2px lightgray"}
-        marginY={"10px"}
-        sx={{
-          cursor: "pointer",
-        }}
-      >
-        <KeyboardBackspaceIcon
-          sx={{
-            bgcolor: `primary.main`,
-            color: `#fff`,
-            borderRadius: "50%",
-            fontSize: "1rem",
-            marginX: "12px",
-          }}
-        />
-        <Typography variant="h6" fontWeight={700}>
-          بند فرعي مثال 2
-        </Typography>
-      </Box>
+      <SingleBtn />
+      <SingleBtn />
+      <SingleBtn />
     </Grid>
   );
 }
