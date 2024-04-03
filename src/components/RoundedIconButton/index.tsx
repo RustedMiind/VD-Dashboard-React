092e64d70ae03da9cdbc1271d58d4f5021159899
rustedmiind <alisolimanworks@gmail.com>
2024-04-03 14:46:17 +0200

    update to RoundedIconButton Border Radius

--- src/components/RoundedIconButton/index.tsx
+++ src/components/RoundedIconButton/index.tsx
@@ -2,7 +2,11 @@ import { Fab, FabProps } from "@mui/material";
 
 function RoundedIconButton(props: FabProps) {
   return (
-    <Fab size={"small"} {...props} sx={{ borderRadius: 0.5, ...props.sx }} />
+    <Fab
+      size={"small"}
+      {...props}
+      sx={{ borderRadius: 0.5, zIndex: 0, ...props.sx }}
+    />
   );
 }
 
