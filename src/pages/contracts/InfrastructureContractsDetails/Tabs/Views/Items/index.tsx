f03d1c01bea7a7b5e892f59445e64026cd81d66a
Sallam Rady <sallamrady@gmail.com>
2024-04-03 15:29:21 +0200

    handle case when there is no items

--- src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/Items/index.tsx
+++ src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/Items/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { ContractDetailsContext } from "../../..";
 import MainPand from "./components/MainPand";
+import { Button, Stack, Typography } from "@mui/material";
 
 export default function ContractItemsDetails() {
   const { contract } = useContext(ContractDetailsContext);
@@ -9,6 +10,16 @@ export default function ContractItemsDetails() {
       {contract?.contract_items?.map((contract) => (
         <MainPand key={contract.contract_id} contractData={contract} />
       ))}
+      {contract?.contract_items?.length == 0 && (
+        <Stack justifyContent={"center"} alignItems={"center"}>
+          <Typography variant="body1" fontWeight={600}>
+            لا يوجد بنود رئيسية
+          </Typography>
+          <Button variant="contained" sx={{ width: "150px", marginY: 2 }}>
+            أضافة بند
+          </Button>
+        </Stack>
+      )}
     </>
   );
 }
