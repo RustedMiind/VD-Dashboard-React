be022554950b471b9cbde86354a15f6928ca894f
Sallam Rady <sallamrady@gmail.com>
2024-04-04 16:57:58 +0200

    last change

--- src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/Items/components/MainPandHeader.tsx
+++ src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/Items/components/MainPandHeader.tsx
@@ -24,6 +24,7 @@ import { CreateTransactionContext } from "../context/CreateTransactionContext";
 
 export default function MainPandHeader(props: MainPandHeaderProps) {
   // define our variables
+  const transactionCxtData = useContext(CreateTransactionContext);
   const [openTimeMap, setOpenTimeMap] = useState(false);
   const [showNotificationsList, setShowNotificationsList] = useState(false);
 
