95ae31a9497127ecee0663328a246768aad684d9
Sallam Rady <sallamrady@gmail.com>
2024-04-07 15:47:32 +0200

    Show Create new Item Btn

--- src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/Items/index.tsx
+++ src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/Items/index.tsx
@@ -5,12 +5,22 @@ import { Button, Stack, Typography } from "@mui/material";
 import { CreateTransactionContextProvider } from "./context/CreateTransactionContext";
 import { TransactionContextProvider } from "./context/TransactionContext";
 import { ReplyTransactionContextProvider } from "./context/ReplyTransactionContext";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 
 export default function ContractItemsDetails() {
   const { contract } = useContext(ContractDetailsContext);
   return (
     <>
       <CreateTransactionContextProvider>
+        <Stack justifyContent={"center"} width={"100%"} alignItems={"end"}>
+          <Button
+            variant="contained"
+            sx={{ width: "150px", marginY: 2 }}
+            startIcon={<AddBoxOutlinedIcon />}
+          >
+            انشاء بند
+          </Button>
+        </Stack>
         {contract?.contract_items?.map((contract) => (
           <TransactionContextProvider key={contract.contract_id}>
             <ReplyTransactionContextProvider>
@@ -23,8 +33,12 @@ export default function ContractItemsDetails() {
             <Typography variant="body1" fontWeight={600}>
               لا يوجد بنود رئيسية
             </Typography>
-            <Button variant="contained" sx={{ width: "150px", marginY: 2 }}>
-              أضافة بند
+            <Button
+              variant="contained"
+              sx={{ width: "150px", marginY: 2 }}
+              startIcon={<AddBoxOutlinedIcon />}
+            >
+              انشاء بند
             </Button>
           </Stack>
         )}
