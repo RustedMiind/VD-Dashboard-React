7655872d62bf56b72176d104afb391d50d03371e
rustedmiind <alisolimanworks@gmail.com>
2024-04-23 14:48:34 +0200

    Select subitem button styles

--- src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/ItemsV2/ItemAccordion/Details/index.tsx
+++ src/pages/contracts/InfrastructureContractsDetails/Tabs/Views/ItemsV2/ItemAccordion/Details/index.tsx
@@ -1,10 +1,11 @@
-import { Button, Grid, List, Stack } from "@mui/material";
+import { Button, Grid, List, Paper, Stack, Typography } from "@mui/material";
 import {
   ContractItem,
   ContractSubItem,
 } from "../../../../../../../../types/Contracts/ContractItems";
 import { useState } from "react";
 import TabsSection from "./Tabs";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function ItemDetails({ item }: PropsType) {
   const [currentSubItem, setCurrentSubItem] = useState<
@@ -15,16 +16,18 @@ function ItemDetails({ item }: PropsType) {
     <Grid container spacing={2}>
       <Grid item xs={12} lg={3}>
         {item.contract_sub_items && (
-          <Stack spacing={0.5}>
+          <Stack spacing={0.5} component={Paper} bgcolor={"background.default"}>
             {item.contract_sub_items.map((subItem) => (
               <Button
                 size="large"
+                sx={{ justifyContent: "start", py: 1.5 }}
                 variant={
                   currentSubItem?.id === subItem.id ? "contained" : "text"
                 }
+                startIcon={<ArrowBackIcon />}
                 onClick={() => setCurrentSubItem(subItem)}
               >
-                {subItem.name}
+                <Typography fontWeight={700}>{subItem.name}</Typography>
               </Button>
             ))}
           </Stack>
