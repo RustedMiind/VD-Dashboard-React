e03b6195cd1c57838f2813d542db8e997a75fd40
Sallam Rady <sallamrady@gmail.com>
2024-07-05 10:52:37 +0300

    handle show tabs in details page according contract type

--- src/pages/contracts/InfrastructureContractsDetails/Tabs/Tabs.tsx
+++ src/pages/contracts/InfrastructureContractsDetails/Tabs/Tabs.tsx
@@ -1,7 +1,10 @@
 import { Chip, ChipProps, Paper, Stack } from "@mui/material";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { TabContext, TabContextValue } from ".";
 import { TabEnum } from "./TabEnum";
+import { ContractDetailsContext } from "../../SetContract/ContractDetailsContext";
+import { useParams } from "react-router-dom";
+import { ContractTyoeAndDataContext } from "../context/ContractTyoeAndData";
 
 function useCreateChipProps({ setTab, tab }: TabContextValue) {
   return function (label: React.ReactNode, thisTab?: TabEnum): ChipProps {
@@ -19,16 +22,32 @@ function useCreateChipProps({ setTab, tab }: TabContextValue) {
 function Tabs() {
   const { tab, setTab } = useContext(TabContext);
   const createChipProps = useCreateChipProps({ tab, setTab });
+  const { ContractType } = useContext(ContractTyoeAndDataContext);
 
   return (
     <Stack component={Paper} gap={1} p={2} direction="row" flexWrap="wrap">
+      {/*  Shared Tabs  */}
       <Chip {...createChipProps("معلومات العقد", TabEnum.DETAILS)} />
-      {/* <Chip {...createChipProps("البنود", TabEnum.ITEMS)} /> */}
       <Chip {...createChipProps("البنود", TabEnum.ITEMS_2)} />
-      <Chip {...createChipProps("المعاملات")} />
-      <Chip {...createChipProps("الحدول الزمني")} />
-      <Chip {...createChipProps("المرفقات", TabEnum.ATTACHMENTS)} />
-      <Chip {...createChipProps("مساحة العمل")} />
+      {/* Infrestructre Contract Tabs */}
+      {ContractType === "Infrestructure" && (
+        <>
+          <Chip {...createChipProps("المعاملات")} />
+          <Chip {...createChipProps("الحدول الزمني")} />
+          <Chip {...createChipProps("المرفقات", TabEnum.ATTACHMENTS)} />
+          <Chip {...createChipProps("مساحة العمل")} />
+        </>
+      )}
+      {/* Standard Contract Tabs */}
+      {ContractType === "Standard" && (
+        <>
+          <Chip {...createChipProps("المرفقات")} />
+          <Chip {...createChipProps("المقاولين")} />
+          <Chip {...createChipProps("أوامر العمل")} />
+          <Chip {...createChipProps("المالية")} />
+          <Chip {...createChipProps("ادرات العقد")} />
+        </>
+      )}
     </Stack>
   );
 }
